60ef324f33011db4ea4accf1c2d50da6f1a070d4
yugurlu <yusufugurlu39@outlook.com>
2023-09-23 17:04:32 +0300

    z

--- frontend/auth_client/src/components/Home.jsx
+++ frontend/auth_client/src/components/Home.jsx
@@ -1,83 +1,85 @@
-import React, { useState } from 'react';
-import '../Home.css';
-import axios from 'axios';
-import { useEffect } from 'react';
-import  SendTweet from './SendTweet'
-import Tweet from './Tweet';
-import * as moment from 'moment';
-import 'moment/locale/pt-br';
+import React, { useState } from "react";
+import "../Home.css";
+import axios from "axios";
+import { useEffect } from "react";
+import SendTweet from "./SendTweet";
+import Tweet from "./Tweet";
+import * as moment from "moment";
+import "moment/locale/pt-br";
 
 function App() {
-  const [tweets, setTweets] = useState(null)
-  const [newTweet, setNewTweet] = useState(null)
-  const [isHovered, setHovered] = useState(false)
-  const [headerText, setHeaderText] = useState("")
+  const [tweets, setTweets] = useState(null);
+  const [newTweet, setNewTweet] = useState(null);
+  const [isHovered, setHovered] = useState(false);
+  const [headerText, setHeaderText] = useState("");
   const [selectedTweetId, setSelectedTweetId] = useState(null);
-  
-  const userProfilePicture = localStorage.getItem("loggedInUserProfilePicture")
-  
+
+  const userProfilePicture = localStorage.getItem("loggedInUserProfilePicture");
+
   useEffect(() => {
     async function getTweets() {
-      const response = await axios.get("http://localhost:3001/tweets", { withCredentials: true })
+      const response = await axios.get("http://localhost:3001/tweets", {
+        withCredentials: true,
+      });
       if (response) {
-        setTweets(response.data)
+        setTweets(response.data);
       }
     }
-    getTweets()
-  }, [])
-  
+    getTweets();
+  }, []);
+
   useEffect(() => {
     if (newTweet) {
-      tweets.reverse().push(newTweet)
-      tweets.reverse()
+      tweets.reverse().push(newTweet);
+      tweets.reverse();
       setTweets(tweets);
-      setNewTweet(null)
+      setNewTweet(null);
     }
   }, [newTweet, tweets]);
 
   useEffect(() => {
-    moment.locale("tr")
-    const time = parseInt(moment().format('LT').substring(0, 2))
-    if (6 < time && time < 12)
-      setHeaderText("Good Morning!")
-    else if (12 <= time && time <= 18)
-      setHeaderText("Good Afternoon!")
-    else
-      setHeaderText("Good Evening!")
-  })
-  
+    moment.locale("tr");
+    const time = parseInt(moment().format("LT").substring(0, 2));
+    if (6 < time && time < 12) setHeaderText("Good Morning!");
+    else if (12 <= time && time <= 18) setHeaderText("Good Afternoon!");
+    else setHeaderText("Good Evening!");
+  });
 
   return (
     <div className="home">
       <header className="header">
         <img
           src="https://static.dezeen.com/uploads/2023/07/x-logo-twitter-elon-musk_dezeen_2364_col_0-1.jpg"
-          alt='Twitter'
+          alt="Twitter"
           className="logo"
-          />
+        />
         <img
           src={userProfilePicture}
-          alt='Profile'
+          alt="Profile"
           className="profile"
           style={{
-            transform: isHovered ? 'scale(1.1)' : 'scale(1)',
-            transition: 'transform 0.3s ease-in-out'
+            transform: isHovered ? "scale(1.1)" : "scale(1)",
+            transition: "transform 0.3s ease-in-out",
           }}
-          onMouseEnter={ () => { setHovered(true) }}
-          onMouseLeave={ () => { setHovered(false) }}
+          onMouseEnter={() => {
+            setHovered(true);
+          }}
+          onMouseLeave={() => {
+            setHovered(false);
+          }}
         />
         <h1 className="headerText">{headerText}</h1>
-       </header>
-       <div>
-        <SendTweet setNewTweet={setNewTweet}/>
-       </div>
+      </header>
+      <div>
+        <SendTweet setNewTweet={setNewTweet} />
+      </div>
       <main className="main">
         {tweets == null ? (
-            <p>No tweets avaiable...</p>
-          ) : (
-            <div className="tweet-list">
-              {tweets.map((tweets, index) => (
-                <Tweet
+          <p>No tweets avaiable...</p>
+        ) : (
+          <div className="tweet-list">
+            {tweets.map((tweets, index) => (
+              <Tweet
                 key={index}
                 id={tweets._id}
                 username={tweets.sharedBy}
@@ -86,13 +88,13 @@ function App() {
                 likes={tweets.Actions.likes}
                 comments={tweets.Actions.comments}
                 isSelected={tweets._id === selectedTweetId}
-                media='https://pbs.twimg.com/media/F6gA5aTXUAASx5Z?format=jpg&name=large'
-                />
-              ))}
-            </div>
+                media="https://pbs.twimg.com/media/F6gA5aTXUAASx5Z?format=jpg&name=large"
+              />
+            ))}
+          </div>
         )}
       </main>
-  </div>
+    </div>
   );
 }
 
--- frontend/auth_client/src/components/SendComment.jsx
+++ frontend/auth_client/src/components/SendComment.jsx
@@ -1,51 +1,62 @@
+import React, { useState } from "react";
+import "../SendComment.css";
+import axios from "axios";
 
-import React, { useState } from 'react';
-import '../SendComment.css';
-import axios from 'axios';
+function SendComment({ setNewTweet }) {
+  const [tweet, setTweet] = useState("");
+  const [isHovered, setHovered] = useState(false);
 
-function SendComment({setNewTweet}) {
-  const [tweet, setTweet] = useState('')
-  const [isHovered, setHovered] = useState(false)
-  
-  const user = localStorage.getItem("loggedInUser")
+  const user = localStorage.getItem("loggedInUser");
 
-    var send = async () => {
-        try {
-          if (tweet.length > 0) {
-            const response = await axios.post("http://localhost:3001/tweets", {
-              username: user,
-              content: tweet
-            }, { withCredentials: true })
-            setTweet('')
-            setNewTweet(response.data);
-          }
-        }
-        catch (error) {
-          console.log(error)
-        }
+  var send = async () => {
+    try {
+      if (tweet.length > 0) {
+        const response = await axios.post(
+          "http://localhost:3001/tweets",
+          {
+            username: user,
+            content: tweet,
+          },
+          { withCredentials: true }
+        );
+        setTweet("");
+        setNewTweet(response.data);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
   return (
     <div className="comment-area">
-        <input
-          type="text"
-          placeholder="Yorum Yap!"
-          className="comment-text-area"
-          value={tweet}
-          onChange={(e) => {setTweet(e.target.value)}}
-        />
-        <p
-            alt="Gonder"
-            className="yanitla"
-            onClick={send}
-            style={{
-              transform: isHovered ? 'scale(1.08)' : 'scale(1)',
-              transition: 'transform 0.3s ease-in-out',
-              fontWeight: 'bold',
-            }}
-            onMouseEnter={() => { setHovered(true) }}
-            onMouseLeave={() => { setHovered(false) }}
-        > Yanıtla</p>
+      <input
+        type="text"
+        placeholder="Yorum Yap!"
+        className="comment-text-area"
+        value={tweet}
+        onChange={(e) => {
+          setTweet(e.target.value);
+        }}
+      />
+      <p
+        alt="Gonder"
+        className="yanitla"
+        onClick={send}
+        style={{
+          transform: isHovered ? "scale(1.08)" : "scale(1)",
+          transition: "transform 0.3s ease-in-out",
+          fontWeight: "bold",
+        }}
+        onMouseEnter={() => {
+          setHovered(true);
+        }}
+        onMouseLeave={() => {
+          setHovered(false);
+        }}
+      >
+        {" "}
+        Yanıtla
+      </p>
     </div>
   );
 }
